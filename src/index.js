42ff308cc4c26c0e281cf0441b8dbaf655a55a46
Artyom <artalar@ya.ru>
2017-12-14 14:59:30 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render } from 'react-dom';
+import FormSimple from './FormSimple';
+import FormSimpleWithValidation from './FormSimpleWithValidation';
+
+const styles = {
+  fontFamily: 'sans-serif',
+  textAlign: 'center',
+};
+
+const App = () => (
+  <div style={styles}>
+    <h2>Simple form</h2>
+    <FormSimple/>
+    <br />
+    <h2>Simple form with validation</h2>
+    <FormSimpleWithValidation />
+  </div>
+);
+
+render(<App />, document.getElementById('root'));
